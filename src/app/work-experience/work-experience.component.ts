d9947725c3432a3ec7f9487d037727dcd3e89483
Marco <marcodaum2@gmail.com>
2022-03-13 18:27:55 +0100

    Changed work info

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -8,8 +8,8 @@ import {Component} from '@angular/core';
 export class WorkExperienceComponent {
 
   events = [
-    {status: 'A-Level', date: 'July 2020'},
-    {status: 'Student at Airbus Defece and Space', date: 'September 2020 - now'},
+    {status: 'Abitur', date: 'July 2020'},
+    {status: 'Dual student at Airbus Defece and Space', date: 'September 2020 - now'},
     {status: 'Software Developer at Schanzer-Racing Electric e.V.', date: 'October 2021 - now'},
     {status: 'Founder of the startup "WoSind"', date: 'November 2021 - now'}
 ];;
