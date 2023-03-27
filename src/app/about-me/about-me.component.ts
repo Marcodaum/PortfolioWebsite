82eb6f9300e1bfef276652de37ab245cbfc8df88
Marcodaum <marco.daum@schanzer-racing.de>
2023-03-27 22:03:30 +0200

    fixed semester calculation and replaces photo

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -9,6 +9,7 @@ export class AboutMeComponent implements OnInit {
   birthday = new Date('2002-08-11T12:00:00');
   age:number = 0;
   startYear = 2020
+  endYear = 2024
   semesterNumber = 0
 
   ngOnInit(): void {
@@ -16,19 +17,20 @@ export class AboutMeComponent implements OnInit {
     this.age = Math.floor((timeDiff / (1000 * 3600 * 24))/365.25);
     
     let currentDate = new Date();
-    for (let i = this.startYear; i <= currentDate.getFullYear(); i++) {
+    for (let i = this.startYear; (i <= currentDate.getFullYear()) && (i <= this.endYear); i++) {
       if (i == this.startYear) {
         this.semesterNumber++;
       } else if (i == currentDate.getFullYear()) {
         if (currentDate.getMonth() >= 8) {
           this.semesterNumber++;
         }
-        if (currentDate.getMonth() >= 3) {
+        if (currentDate.getMonth() >= 2) {
           this.semesterNumber++;
         }
       } else {
         this.semesterNumber += 2;
       }
     }
+    console.log(currentDate.getMonth())
   }
 }
