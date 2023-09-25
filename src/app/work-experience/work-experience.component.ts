1a6c3f98a20e65e7886848ff284597f4561de938
Marcodaum <marco.daum@schanzer-racing.de>
2023-09-25 10:45:40 +0200

    updated work experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -11,7 +11,6 @@ export class WorkExperienceComponent {
     {status: 'Abitur', date: 'July 2020'},
     {status: 'Dual student at Airbus Defence and Space', date: 'September 2020 - now'},
     {status: 'Software Developer at Schanzer-Racing Electric e.V.', date: 'October 2021 - now'},
-    {status: 'Founder of the startup "WoSind"', date: 'November 2021 - now'},
     {status: 'Intern at Airbus FAL USA', date: 'September 2022 - December 2022'}
 ];
 
